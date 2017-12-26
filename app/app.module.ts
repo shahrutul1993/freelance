339e98b89139465e5710a59be57ddffbdec2391e
Shah RUtul <shahrutul111@gmail.com>
2017-12-26 08:30:52 +0530

    angular app

---
+++ app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+//import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { DocumentsComponent } from './documents/documents.component';
+import { ProposalListComponent } from './proposal/proposal-list.component';
+import { DocumentService } from './documents/document.service';
+import { ProposalShowComponent } from './proposal/proposal-show.component';
+import { ProposalNewComponent } from './proposal/proposal-new.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ProposalService } from './proposal/proposal.service';
+
+@NgModule({
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		FormsModule,
+		//NgbModule.forRoot()
+		HttpModule
+	],
+	declarations: [
+		AppComponent,
+		HomepageComponent,
+		DocumentsComponent,
+		ProposalListComponent,
+		ProposalNewComponent,
+		ProposalShowComponent
+	],
+	providers: [
+		DocumentService
+		ProposalService
+	],
+	bootstrap: [
+		AppComponent
+	]
+})
+export class AppModule {}
---
+++ app/proposal/proposal-list.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Proposal } from './proposal';
+import { Observable } from 'rxjs/Rx';
+import { ProposalService } from './proposal.service';
+import { Router } from '@angular/router';
+
+
+@Component({
+	moduleId: module.id,
+	selector: 'proposal-list',
+	templateUrl: 'proposal-list.component.html',
+	providers: [ProposalService]
+})
+export class ProposalListComponent implements OnInit{
+	proposals: Proposal[];
+	errorMessage: string;
+	mode = "Observable";
+
+
+	constructor(
+		private proposalService: ProposalService,
+		private router: Router
+		){}
+	ngOnInit() {
+		let timer = Observable.timer(0,5000)
+		timer.subscribe(() => this.getProposals());
+	}
+
+
+	getProposals(){
+		this.proposalService.getProposals()
+			.subscribe(
+				proposals => this.proposals =  proposals,
+				error => this.errorMessage = <any>error
+			);
+	}
+
+	goToShow(proposal: Proposal): void {
+		let link = ['/proposal', proposal.id];
+		this.router.navigate(link);
+	}
+	
+}
